d3678029cddc2b602934ecd6ac8b5176970a7615
XArtola <xartolaza18dw@ikzubirimanteo.com>
2019-09-29 12:59:15 +0200

    He añadido ejercicio1

---
+++ Ejercicio1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejercicio1</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+	<?php
+
+	$naturales =[0,1,2,3,4,5,6,7,8,9];
+	$factoriales = [0,1,1,1,1,1,1,1,1,1];
+
+	for ($i = 0; $i < count($naturales); $i = $i + 1) { 
+
+		for ($j = 1; $j <= $naturales[$i] ; $j = $j + 1)
+			$factoriales[$i] *= $j;
+
+	}
+
+	print_r($factoriales)
+	?>
+
+</body>
+</html>
