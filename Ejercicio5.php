e5fa11de718c533ae5d8416dd442fc590db48d63
XArtola <xartolaza18dw@ikzubirimanteo.com>
2019-09-29 12:59:55 +0200

    He añadido ejercicio5

---
+++ Ejercicio5.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejercicio5</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+	<?php
+
+	$a = array (
+		"Enero" => 31,
+		"Febrero" => 28,
+		"Marzo" => 31,
+		"Abril" => 30,
+		"Mayo" => 31,
+		"Junio" => 30,
+		"Julio" => 31,
+		"Agosto" => 31,
+		"Septiembre" => 30,
+		"Octubre" => 31,
+		"Noviembre" => 30,
+		"Diciembre" => 31
+	);
+
+	print_r($a);
+	?>
+
+</body>
+</html>
