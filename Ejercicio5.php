efe5287139855c3fe4815721fa4ee35a292c3da7
xabi <xartolaza18dw@ikzubirimanteo.com>
2019-09-30 13:56:45 +0200

    corrección de errores

--- Ejercicio5.php
+++ Ejercicio5.php
@@ -1,30 +1,42 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Ejercicio5</title>
 	<meta charset="utf-8">
 </head>
+
 <body>
 
 	<?php
 
-	$a = array (
-		"Enero" => 31,
-		"Febrero" => 28,
-		"Marzo" => 31,
-		"Abril" => 30,
-		"Mayo" => 31,
-		"Junio" => 30,
-		"Julio" => 31,
-		"Agosto" => 31,
-		"Septiembre" => 30,
-		"Octubre" => 31,
-		"Noviembre" => 30,
-		"Diciembre" => 31
+	$an = 2020;
+
+	$a = array(
+		"Enero" => "",
+		"Febrero" => "",
+		"Marzo" => "",
+		"Abril" => "",
+		"Mayo" => "",
+		"Junio" => "",
+		"Julio" => "",
+		"Agosto" => "",
+		"Septiembre" => "",
+		"Octubre" => "",
+		"Noviembre" => "",
+		"Diciembre" => ""
 	);
 
+	$i=1;
+	foreach($a as $clave => $valor){
+
+		$a[$clave] = cal_days_in_month(CAL_GREGORIAN, $i, $an);
+		$i++;
+	}
+	
 	print_r($a);
 	?>
 
 </body>
+
 </html>
