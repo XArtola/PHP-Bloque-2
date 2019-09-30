46da74d5cd13c154878488e737efb3cc43ffd4dd
xabi <xartolaza18dw@ikzubirimanteo.com>
2019-09-30 13:54:07 +0200

    He cambiado la generación de arrays

--- Ejercicio1.php
+++ Ejercicio1.php
@@ -1,25 +1,36 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Ejercicio1</title>
 	<meta charset="utf-8">
 </head>
+
 <body>
 
 	<?php
 
-	$naturales =[0,1,2,3,4,5,6,7,8,9];
-	$factoriales = [1,1,1,1,1,1,1,1,1,1];
+	$naturales = array();
+	$factoriales = array();
 
-	for ($i = 0; $i < count($naturales); $i = $i + 1) { 
+	for ($i = 0; $i < 10; $i++) {
 
-		for ($j = 1; $j <= $naturales[$i] ; $j = $j + 1)
-			$factoriales[$i] *= $j;
-
+		array_push($naturales, $i);
+		array_push($factoriales, factorial($i));
 	}
 
-	print_r($factoriales)
+
+	function factorial($numero)
+	{
+		$aux = 1;
+		for ($j = 1; $j <= $numero; $j++)
+			$aux *= $j;
+
+		return $aux;
+	}
+	print_r($factoriales);
 	?>
 
 </body>
+
 </html>
