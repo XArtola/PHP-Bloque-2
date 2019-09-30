c5ffe7fa74082410481371c9330cacd23f40c476
xabi <xartolaza18dw@ikzubirimanteo.com>
2019-09-30 13:35:35 +0200

    Ejercicio actualizado

--- Ejercicio1.php
+++ Ejercicio1.php
@@ -9,7 +9,7 @@
 	<?php
 
 	$naturales =[0,1,2,3,4,5,6,7,8,9];
-	$factoriales = [0,1,1,1,1,1,1,1,1,1];
+	$factoriales = [1,1,1,1,1,1,1,1,1,1];
 
 	for ($i = 0; $i < count($naturales); $i = $i + 1) { 
 
